f126fdb71faa4a495e292008eb6dee7dce414376
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-19 20:14:18 -0300

    Add properties

--- src/entities/Libro.js
+++ src/entities/Libro.js
@@ -1,2 +1,25 @@
 class Libro{
+
+    #cantPags;
+
+    //No existen los constructores por default en JS:
+    /*
+    constructor(){
+        cantPags = 0;
+    }
+    */
+
+    constructor(cantPags){
+        setCantPags(cantPags);
+    }
+
+    setCantPags(cantPags){
+        this.cantPags = cantPags;
+    }
+
+    getCantPags(){
+        return this.cantPags;
+    }
+
+    //export{ getCantPags};
 }
