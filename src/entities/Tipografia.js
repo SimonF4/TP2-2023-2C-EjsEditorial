0b3baad046c500da7b859f2f7a77008b66820ad3
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-19 19:27:03 -0300

    Add properties

--- src/entities/Tipografia.js
+++ src/entities/Tipografia.js
@@ -1,2 +1,11 @@
 class Tipografia{
+    #alto // : Medida
+    #ancho // : Medida
+    #espaciado //: Medida
+    
+    constructor(alto, ancho, espaciado){
+        this.alto = alto;
+        this.ancho = ancho;
+        this.espaciado = espaciado;
+    }
 }
