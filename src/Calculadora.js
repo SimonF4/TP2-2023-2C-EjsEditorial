a42e6e338d090dd1bcaae0784aedd1498bcf5536
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-19 20:20:03 -0300

    Add instances for testing

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -1,1 +1,7 @@
 //Clase principal.
+
+    let medidaDeTest = new Medida(1.20, "pixeles");
+    let tipografia01 = new Tipografia(new Medida(), new Medida(), new Medida());
+    let linea = new Linea(new Medida(), new Medida(), 7, new Medida());
+    let pagina = new Pagina(7, new Medida(), new Medida(), new Medida(), new Medida());
+    let libro = new Libro(150);
