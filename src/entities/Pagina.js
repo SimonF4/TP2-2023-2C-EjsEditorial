da0db104367abf40b898dfc30233381bb8cde1cb
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-19 19:21:27 -0300

    Add properties

--- src/entities/Pagina.js
+++ src/entities/Pagina.js
@@ -1,2 +1,23 @@
 class Pagina {
+    #cantidadDeLineas //: number
+    #margenSuperior //: Medida
+    #margenInferior //: Medida
+    #margenDerecho //: Medida
+    #margenIzquierdo //: Medida
+
+    constructor(cantidadDeLineas, margenSuperior, margenInferior, margenDerecho, margenIzquierdo){
+        this.setCantLineas(cantidadDeLineas);
+        this.margenSuperior = margenSuperior;
+        this.margenInferior = margenInferior;
+        this.margenDerecho = margenDerecho;
+        this.margenIzquierdo = margenIzquierdo;
+    }
+
+    setCantLineas(cantidadDeLineas){
+        if(typeof cantidadDeLineas != Number){
+            throw new Error('La cantidad de lineas debe ser un dato numerico');
+        }
+
+        this.#cantidadDeLineas = cantidadDeLineas;
+    }
 }
