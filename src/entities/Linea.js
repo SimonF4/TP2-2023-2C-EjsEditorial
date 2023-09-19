7591d590c79d4d07a76e4f3016c69ff6dc112316
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-19 17:19:47 -0300

    Add properties

--- src/entities/Linea.js
+++ src/entities/Linea.js
@@ -1,2 +1,18 @@
 class Linea {
+
+    #alto //: Medida
+    #ancho //: Medida
+    #cantidadDeCaracteres //: number
+    #interlineado //: Medida
+
+    constructor(alto, ancho, cantidadDeCaracteres, interlineado){
+        this.alto = alto;
+        this.ancho = ancho;
+        setCantidadCaracteres(cantidadDeCaracteres);
+        this.cantidadDeLineas = cantidadDeLineas;
+    }
+
+    setCantidadCaracteres(cantidadDeCaracteres){
+        this.cantidadDeCaracteres = cantidadDeCaracteres;
+    }
 }
