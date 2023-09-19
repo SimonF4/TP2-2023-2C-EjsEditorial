5b47a3d6788e289a0d44fe45999dcae05b53ff0b
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-19 17:05:04 -0300

    Add properties to class Medida

--- src/entities/Medida.js
+++ src/entities/Medida.js
@@ -1,3 +1,9 @@
 class Medida{
+    #medida // : float
+    #unidadDeMedida // (pixels, milímetros) : string
     
+    constructor(medida, unidadDeMedida){
+        this.#medida = medida;
+        this.#unidadDeMedida = unidadDeMedida;
+    }
 }
