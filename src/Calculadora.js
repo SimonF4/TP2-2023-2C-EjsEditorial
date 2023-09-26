a5336000e15c6122f1d3d794efbd5b9ee5609e94
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-26 17:37:28 -0300

    Complete test example values

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -9,10 +9,10 @@ const {
 } = require('./src/entities')
 
 try {
-    let medidaDeTest = new Medida(1.20, "pixeles");
-    let tipografia01 = new Tipografia(new Medida(), new Medida(), new Medida());
-    let linea = new Linea(new Medida(), new Medida(), 7, new Medida());
-    let pagina = new Pagina(7, new Medida(), new Medida(), new Medida(), new Medida());
+    let medidaDeTest = new Medida(200, "pixeles");
+    let tipografia01 = new Tipografia(new Medida(8, "pixeles"), new Medida(3, "pixeles"), new Medida(5, "pixeles"));
+    let linea = new Linea(new Medida(1600, "pixeles"), new Medida(400, "pixeles"), 200, new Medida(5));
+    let pagina = new Pagina(15, new Medida(10, "milimetros"), new Medida(10, "milimetros"), new Medida(10, "milimetros"), new Medida(10, "milimetros"));
     let libro = new Libro(150);
 
     alert(libro.getCantPags());
