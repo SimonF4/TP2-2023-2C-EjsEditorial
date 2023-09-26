713b169ca034b9ad9cf8a3b0d0e348a70dca06be
SimonF4 <simonetti_francoandres@hotmail.com>
2023-09-26 17:03:03 -0300

    Add setters

--- src/entities/Tipografia.js
+++ src/entities/Tipografia.js
@@ -6,15 +6,33 @@ class Tipografia{
     #espaciado //: Medida
     
     constructor(alto, ancho, espaciado){
-        this.alto = alto;
-        this.ancho = ancho;
-        this.espaciado = espaciado;
+        this.setAlto(alto);
+        this.setAncho(ancho);
+        this.setEspaciado(espaciado);
     }
 
     validarMedida(medida){
         if(typeof medida != Medida){
             throw new Error('El dato ingresado debe ser de tipo Medida');
         }
+    }
+
+    setAlto(alto){
+        this.validarMedida(alto);
+
+        this.#alto = alto;
+    }
+
+    setAncho(ancho){
+        this.validarMedida(ancho);
+
+        this.#ancho = ancho;
+    }
+
+    setEspaciado(espaciado){
+        this.validarMedida(espaciado);
+
+        this.#espaciado = espaciado;
     }
 }
 
